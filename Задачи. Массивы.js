b58149de2f9be805ec4781da1c7d04a5b3b27082
Friedel <ekaterina.friedel@gmail.com>
2023-04-08 22:08:40 +0300

    Задачи. Массивы

---
+++ Задачи. Массивы.js
@@ -0,0 +1,23 @@
+// Задача. Операции с массивами 
+function sumInput() {
+
+    let numbers = [];
+  
+    while (true) {
+  
+      let value = prompt("Введите число", 0);
+  
+      // Надоело?
+      if (value === "" || value === null || !isFinite(value)) break;
+  
+      numbers.push(+value);
+    }
+  
+    let sum = 0;
+    for (let number of numbers) {
+      sum += number;
+    }
+    return sum;
+  }
+  
+  alert( sumInput() );
