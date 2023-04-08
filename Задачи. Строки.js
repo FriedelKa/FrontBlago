c5fbf232b827deacbf44a58d3d003c2f13ffd013
Friedel <ekaterina.friedel@gmail.com>
2023-04-08 14:30:52 +0300

    Задачи. Строки

---
+++ Задачи. Строки.js
@@ -0,0 +1,31 @@
+// Задача. Сделать первый символ заглавным
+function ucFirst(str) {
+    if (!str) return str;
+  
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  
+  alert( ucFirst("вася") );
+
+  // Задача. Проверка на спам
+  function checkSpam(str) {
+    let lowerStr = str.toLowerCase();
+  
+    return lowerStr.includes('viagra') || lowerStr.includes('xxx');
+  }
+  
+  alert( checkSpam('buy ViAgRA now') );
+  alert( checkSpam('free xxxxx') );
+  alert( checkSpam("innocent rabbit") ); 
+
+  // Задача. Усечение строки 
+  function truncate(str, maxlength) {
+    return (str.length > maxlength) ?
+      str.slice(0, maxlength - 1) + '…' : str;
+  }
+
+  truncate("Вот, что мне хотелось бы сказать на эту тему:", 22);
+  truncate("Всем привет!", 20);  
+
+
+
